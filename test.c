f53e729ac0c49ca96415f355fe24a6da023902e5
Chuck Jungmann <chuckj60@gmail.com>
2020-06-05 12:40:33 -0500

    Added several options.

--- test.c
+++ test.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include "clargs.h"
 
+typedef void (*announcer_t)(void);
+
+void Polite_Announce(void) { printf("Pleased to meet you.\n"); }
+void Rude_Announce(void) { printf("What are you looking at?\n"); }
+
 int intval = 0;
 const char* filename = "/var/log/syslog";
 int cflag = 0;
 int show_help = 0;
+announcer_t Announcer = Polite_Announce;
+
+void Announce_Setter(const DefLine *option, const char *value)
+{
+   printf("Announce setter for %s\n", option->help);
+   Announcer = (announcer_t)option->target;
+}
+
 
 DefLine defs[] = {
-   { 'h', "Show this help.",       &show_help, NULL },
-   { 'v', "Set an integer value.", &intval,    clargs_set_int },
-   { 'f', "Set filename.",         &filename,  clargs_set_string },
-   { 'c', "Set c flag.",           &cflag,     NULL },
+   { 'h', "Show this help.",          &show_help,      NULL },
+   { 'v', "Set an integer value.",    &intval,         clargs_set_int },
+   { 'f', "Set filename.",            &filename,       clargs_set_string },
+   { 'c', "Set c flag.",              &cflag,          NULL },
+   { 'p', "Use polite announcement.", Polite_Announce, Announce_Setter },
+   { 'r', "Use rude announcement.",   Rude_Announce,   Announce_Setter },
    CLARGS_END_DEF
 };
 
@@ -22,6 +37,9 @@ int main(int argc, const char **argv)
       clargs_show(defs);
    else
    {
+      (*Announcer)();
+      printf("\n");
+
       printf("intval = %d.\n"
              "filename = %s.\n"
              "cflag = %d.\n",
